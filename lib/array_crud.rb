4146bdb7da83c3613152d7a8a94df64425e150c1
Chance Smith <chancesmith97@hotmail.com>
2019-01-31 04:48:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  lesson = ["wow", "I", "am", "really", "learning"]
-  lesson.push("arrays!")
+  
+  array.push(element)
   puts array.inspect
 end
 
