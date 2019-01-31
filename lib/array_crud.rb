3a7dc84e1fbc34ddfd0194d1a5cc6246d9f48930
Chance Smith <chancesmith97@hotmail.com>
2019-01-31 04:51:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ end
 def add_element_to_end_of_array(array, element)
   my_array = ["wow", "I", "am", "really", "learning"]
   my_array.push("arrays!")
-  puts my_array.inspect
+  puts my_array
 end
 
 def add_element_to_start_of_array(array, element)
